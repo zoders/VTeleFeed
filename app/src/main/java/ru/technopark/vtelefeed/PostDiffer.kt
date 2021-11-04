0e30b22da19e55fcff7d819ad1d93b22553c7ee4
n.ryzhkov <n.ryzhkov@corp.mail.ru>
2021-11-04 19:16:27 +0300

    Revert "Merge pull request #4 from zoders/klim"
    This reverts commit dd548874651ebb31079ad26ada147a93023176d0, reversing
    changes made to 4aa3747df0253671a70efae99e6f4be5b144a340.

--- app/src/main/java/ru/technopark/vtelefeed/PostDiffer.kt
+++
@@ -1,8 +0,0 @@
-package ru.technopark.vtelefeed
-
-import androidx.recyclerview.widget.DiffUtil
-
-class PostDiffer : DiffUtil.ItemCallback<Post>() {
-    override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean = oldItem?.id == newItem?.id
-    override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean = oldItem == newItem
-}
