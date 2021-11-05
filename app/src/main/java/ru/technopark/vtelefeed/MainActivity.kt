054eba79c1ef9d20a8acb319a2896c2edd2a8390
Djambulat <Djambulat69@gmail.com>
2021-11-06 00:17:03 +0300

    pr fixes

--- app/src/main/java/ru/technopark/vtelefeed/MainActivity.kt
+++ app/src/main/java/ru/technopark/vtelefeed/MainActivity.kt
@@ -1,7 +1,6 @@
 package ru.technopark.vtelefeed
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
-
-
 }
