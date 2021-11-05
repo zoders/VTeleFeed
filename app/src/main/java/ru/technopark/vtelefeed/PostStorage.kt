5ad30a97ac9d696e1aca9cbcbdfba5134affbc08
vinklim <vinklim2000@gmail.com>
2021-11-05 14:12:16 +0300

    .

--- app/src/main/java/ru/technopark/vtelefeed/PostStorage.kt
+++ app/src/main/java/ru/technopark/vtelefeed/PostStorage.kt
@@ -12,12 +12,14 @@ class PostStorage : ViewModel() {
             val post = Post()
             post.id = i
             post.fromVkOrTg = if (i % 2 == 0) R.drawable.vk else R.drawable.tg
-            for (j in 0..PEREMENNAYA_S_PONYATNYM_NAZVANIEM_SPECIALNO_DLYA_STATIC_ANALYSIS) post.text += "текст поста "
+            for (j in 0..PEREMENNAYA_S_PONYATNYM_NAZVANIEM_SPECIALNO_DLYA_STATIC_ANALYSIS)
+			    post.text += "текст поста "
             posts += post
         }
     }
 
     fun getData(startPosition: Int, loadSize: Int): List<Post> =
-        if (startPosition == PEREMENNAYA_S_PONYATNYM_NAZVANIEM_SPECIALNO_DLYA_STATIC_ANALYSIS) emptyList()
+        if (startPosition == PEREMENNAYA_S_PONYATNYM_NAZVANIEM_SPECIALNO_DLYA_STATIC_ANALYSIS)
+		    emptyList()
         else posts.subList(startPosition, startPosition + loadSize)
 }
