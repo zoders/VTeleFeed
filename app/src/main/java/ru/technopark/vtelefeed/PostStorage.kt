a194540993901edb5abdb539153dd5bef28e4551
vinklim <vinklim2000@gmail.com>
2021-11-05 14:10:51 +0300

    .

--- app/src/main/java/ru/technopark/vtelefeed/PostStorage.kt
+++ app/src/main/java/ru/technopark/vtelefeed/PostStorage.kt
@@ -2,20 +2,22 @@ package ru.technopark.vtelefeed
 
 import androidx.lifecycle.ViewModel
 
+private const val PEREMENNAYA_S_PONYATNYM_NAZVANIEM_SPECIALNO_DLYA_STATIC_ANALYSIS = 20
+
 class PostStorage : ViewModel() {
     val posts = mutableListOf<Post>()
 
     init {
-        for (i in 0..20) {
+        for (i in 0..PEREMENNAYA_S_PONYATNYM_NAZVANIEM_SPECIALNO_DLYA_STATIC_ANALYSIS) {
             val post = Post()
             post.id = i
             post.fromVkOrTg = if (i % 2 == 0) R.drawable.vk else R.drawable.tg
-            for (j in 0..20) post.text += "текст поста "
+            for (j in 0..PEREMENNAYA_S_PONYATNYM_NAZVANIEM_SPECIALNO_DLYA_STATIC_ANALYSIS) post.text += "текст поста "
             posts += post
         }
     }
 
     fun getData(startPosition: Int, loadSize: Int): List<Post> =
-        if (startPosition == 20) emptyList()
+        if (startPosition == PEREMENNAYA_S_PONYATNYM_NAZVANIEM_SPECIALNO_DLYA_STATIC_ANALYSIS) emptyList()
         else posts.subList(startPosition, startPosition + loadSize)
 }
