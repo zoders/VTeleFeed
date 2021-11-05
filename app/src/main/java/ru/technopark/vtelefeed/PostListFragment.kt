ed49bee876e838e6bbfd3098ebfd08b4e46ac968
vinklim <vinklim2000@gmail.com>
2021-11-05 13:25:43 +0300

    .

--- app/src/main/java/ru/technopark/vtelefeed/PostListFragment.kt
+++ app/src/main/java/ru/technopark/vtelefeed/PostListFragment.kt
@@ -20,7 +20,11 @@ class PostListFragment : Fragment() {
         ViewModelProvider(this).get(PostStorage::class.java)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+	    inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
         val view = inflater.inflate(R.layout.fragment_post_list, container, false)
         recyclerView = view.findViewById(R.id.recycler_view) as RecyclerView
         recyclerView.layoutManager = LinearLayoutManager(context)
