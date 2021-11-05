f044b98b2e8f5e824836103fdddbf0f65b7dbae7
vinklim <vinklim2000@gmail.com>
2021-11-05 12:54:22 +0300

    .

--- app/src/main/java/ru/technopark/vtelefeed/MainActivity.kt
+++ app/src/main/java/ru/technopark/vtelefeed/MainActivity.kt
@@ -10,8 +10,10 @@ class MainActivity : AppCompatActivity() {
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
         if (currentFragment == null) {
             val fragment = PostListFragment.newInstance()
-            supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment)
-				.commit()
+            supportFragmentManager.beginTransaction().add(
+				R.id.fragment_container, 
+				fragment
+			).commit()
         }
     }
 }
--- app/src/main/java/ru/technopark/vtelefeed/Post.kt
+++ app/src/main/java/ru/technopark/vtelefeed/Post.kt
@@ -3,6 +3,8 @@ package ru.technopark.vtelefeed
 import java.util.Date
 
 data class Post(
-	var id: Int = 0, var fromVkOrTg: Int = 0, var text: String = "", 
+	var id: Int = 0,
+	var fromVkOrTg: Int = 0,
+	var text: String = "",
 	var date: Date = Date()
 )
