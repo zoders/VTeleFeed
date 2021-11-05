f6646a6c634585311998cd2aa71aa7b62c778438
Djambulat <Djambulat69@gmail.com>
2021-11-05 23:03:38 +0300

    cleaned files; removed rxjava

--- app/src/main/java/ru/technopark/vtelefeed/TelegramDataSource.kt
+++ app/src/main/java/ru/technopark/vtelefeed/TelegramDataSource.kt
@@ -1,7 +1,5 @@
 package ru.technopark.vtelefeed
 
-import io.reactivex.rxjava3.core.Maybe
-import io.reactivex.rxjava3.core.Observable
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.withContext
@@ -13,53 +11,6 @@ import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 class TelegramDataSource(private val client: Client) {
-
-    fun getChannelMessagesSingle(
-        query: String,
-        offsetDate: Int = 0,
-        offsetChatId: Long = 0L,
-        offsetMessageId: Long = 0L,
-        limit: Int = 10,
-        filter: TdApi.SearchMessagesFilter = TdApi.SearchMessagesFilterEmpty(),
-        minDate: Int = 0,
-        maxDate: Int = 0
-    ): Maybe<List<TdApi.Message>> {
-        var haveMoreMessages: Boolean
-
-        return client.sendSingle(
-            TdApi.SearchMessages(
-                TdApi.ChatListMain(),
-                query,
-                offsetDate,
-                offsetChatId,
-                offsetMessageId,
-                limit,
-                filter,
-                minDate,
-                maxDate
-            )
-        )
-            .flatMapObservable { response ->
-                Observable.fromArray(*(response as TdApi.Messages).messages)
-            }
-            .flatMap<Pair<TdApi.Message, TdApi.Chat>> { message ->
-
-                val chatObservable =
-                    client.sendSingle(TdApi.GetChat(message.chatId)).toObservable()
-
-                return@flatMap Observable.fromArray(message)
-                    .zipWith(chatObservable)
-                    { zipMessage, zipChat -> zipMessage to (zipChat as TdApi.Chat) }
-            }
-            .filter { pair ->
-                val chatType = pair.second.type
-                chatType is TdApi.ChatTypeSupergroup && chatType.isChannel
-            }
-            .map { it.first }
-
-            .toList()
-            .filter { it.isNotEmpty() }
-    }
 
     suspend fun getChannelsMessages(
         limit: Int,
