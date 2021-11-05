07b8d3626fab9d519424e95ef5e9a67656962b3f
vinklim <vinklim2000@gmail.com>
2021-11-05 13:48:55 +0300

    .

--- app/src/main/java/ru/technopark/vtelefeed/PostStorage.kt
+++ app/src/main/java/ru/technopark/vtelefeed/PostStorage.kt
@@ -6,11 +6,11 @@ class PostStorage : ViewModel() {
     val posts = mutableListOf<Post>()
 
     init {
-        for (i in 0..20) {
+        for (i in 0 .. 20) {
             val post = Post()
             post.id = i
             post.fromVkOrTg = if (i % 2 == 0) R.drawable.vk else R.drawable.tg
-            for (j in 0..20) post.text += "текст поста "
+            for (j in 0 .. 20) post.text += "текст поста "
             posts += post
         }
     }
