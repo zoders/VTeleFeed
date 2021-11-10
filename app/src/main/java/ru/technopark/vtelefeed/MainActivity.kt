061ed67cd6c19b22ac5d9c3aa5a7214d4aa45f5a
ILYA DEYDYSH <kp50040@gmail.com>
2021-11-10 18:56:25 +0300

    rearrenge and finish up MainActivity

---
+++ app/src/main/java/ru/technopark/vtelefeed/MainActivity.kt
@@ -0,0 +1,85 @@
+package ru.technopark.vtelefeed
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.VKApiCallback
+import ru.technopark.vtelefeed.databinding.ActivityMainBinding
+
+/**
+ * Created by Ilya Deydysh on 09.11.2021.
+ */
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        if (VK.isLoggedIn()) {
+            val user: VKUser
+            mainActivityBinding.imbuttonVkLogout.visibility = View.VISIBLE
+            mainActivityBinding.buttonNameVk.visibility = View.VISIBLE
+            mainActivityBinding.buttonLoginVk.visibility = View.GONE
+            requestVKUser()
+        } else {
+            mainActivityBinding.imbuttonVkLogout.visibility = View.INVISIBLE
+            mainActivityBinding.buttonNameVk.visibility = View.GONE
+            mainActivityBinding.buttonLoginVk.visibility = View.VISIBLE
+        }
+
+        setContentView(mainActivityBinding.root)
+
+        mainActivityBinding.buttonLoginVk.setOnClickListener {
+            VKLoginActivity.startFrom(this@MainActivity)
+        }
+
+        mainActivityBinding.imbuttonVkLogout.setOnClickListener {
+            VK.logout()
+            startFrom(this)
+            finish()
+        }
+    }
+
+    companion object {
+        fun startFrom(context: Context) {
+            val intent = Intent(context, MainActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
+    fun requestVKUser() {
+        VK.execute(VKUserCommand(), object : VKApiCallback<VKUser> {
+            override fun success(result: VKUser) {
+                val vkNameButton = findViewById<TextView>(R.id.button_name_vk)
+                vkNameButton.text = "${result.firstName} ${result.lastName}"
+                val vkAvatarIV = findViewById<ImageView>(R.id.image_vk_profile_pic)
+
+                if (!TextUtils.isEmpty(result.photo)) {
+                    Glide.with(this@MainActivity)
+                        .load(result.photo)
+                        .transform(RoundedCorners(16))
+                        .error(R.drawable.user_placeholder)
+                        .into(vkAvatarIV)
+                } else {
+                    vkAvatarIV.setImageResource(R.drawable.user_placeholder)
+                }
+            }
+
+            override fun fail(error: Exception) {
+                Log.e(TAG, error.toString())
+            }
+        })
+    }
+}
