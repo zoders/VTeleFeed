997b35ad73054450ea9c296a0360605223885c6f
ILYA DEYDYSH <kp50040@gmail.com>
2021-11-10 19:27:13 +0300

    code clean up

--- app/src/main/java/ru/technopark/vtelefeed/VTeleFeedApplication.kt
+++ app/src/main/java/ru/technopark/vtelefeed/VTeleFeedApplication.kt
@@ -4,13 +4,13 @@ import android.app.Application
 import com.vk.api.sdk.VK
 import com.vk.api.sdk.VKTokenExpiredHandler
 
-class VTeleFeedApplication: Application() {
+class VTeleFeedApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         VK.addTokenExpiredHandler(tokenTracker)
     }
 
-    private val tokenTracker = object: VKTokenExpiredHandler {
+    private val tokenTracker = object : VKTokenExpiredHandler {
         override fun onTokenExpired() {
             MainActivity.startFrom(this@VTeleFeedApplication)
         }
