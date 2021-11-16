409bc05064bfb41d29913ed81dc3b3bf7baaf7ea
redch <sanyafil61802@gmail.com>
2021-11-16 16:30:37 +0300

    Added Telegram Authorization Fragment 2

--- app/src/main/java/ru/technopark/vtelefeed/MainActivity.kt
+++ app/src/main/java/ru/technopark/vtelefeed/MainActivity.kt
@@ -1,13 +1,28 @@
 package ru.technopark.vtelefeed
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.technopark.vtelefeed.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // some shit
+
+        val mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mainActivityBinding.root)
+
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+
+        fun startFrom(context: Context) {
+            context.startActivity(Intent(context, MainActivity::class.java))
+        }
     }
 }
