352a20f373325db0971cba9a5302fe552484ab5a
Djambulat <Djambulat69@gmail.com>
2021-11-16 15:59:07 +0300

    revert tg auth merge

---
+++ app/src/main/java/ru/technopark/vtelefeed/VKUser.kt
@@ -0,0 +1,55 @@
+package ru.technopark.vtelefeed
+
+import android.os.Parcel
+import android.os.Parcelable
+import org.json.JSONObject
+
+/**
+ * Created by Ilya Deydysh on 09.11.2021.
+ */
+data class VKUser(
+    val id: Long = 0,
+    val firstName: String = "",
+    val lastName: String = "",
+    val photo: String = "",
+    val deactivated: Boolean = false
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readLong(),
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readByte() != 0.toByte()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeLong(id)
+        parcel.writeString(firstName)
+        parcel.writeString(lastName)
+        parcel.writeString(photo)
+        parcel.writeByte(if (deactivated) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<VKUser> {
+        override fun createFromParcel(parcel: Parcel): VKUser {
+            return VKUser(parcel)
+        }
+
+        override fun newArray(size: Int): Array<VKUser?> {
+            return arrayOfNulls(size)
+        }
+
+        fun parse(json: JSONObject) = VKUser(
+            id = json.optLong("id", 0),
+            firstName = json.optString("first_name", ""),
+            lastName = json.optString("last_name", ""),
+            photo = json.optString("photo_50", ""),
+            deactivated = json.optBoolean("deactivated", false)
+        )
+    }
+}
