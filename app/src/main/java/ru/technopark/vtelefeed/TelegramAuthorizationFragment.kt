7c813a19a7c3a02334c197d97fe7384d7c7b9d85
redch <sanyafil61802@gmail.com>
2021-11-16 19:13:12 +0300

    Now with tg authorization (TAF for ktlint)

--- app/src/main/java/ru/technopark/vtelefeed/TelegramAuthorizationFragment.kt
+++ app/src/main/java/ru/technopark/vtelefeed/TelegramAuthorizationFragment.kt
@@ -5,7 +5,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.bumptech.glide.Glide
