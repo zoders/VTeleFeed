64ea54d8b3274fe289eba1ad4e8cc482b757067b
Djambulat <Djambulat69@gmail.com>
2021-12-02 18:40:17 +0300

    reviewdog fixes

--- app/src/main/java/ru/technopark/vtelefeed/ui/postlist/PostPhotoHolder.kt
+++ app/src/main/java/ru/technopark/vtelefeed/ui/postlist/PostPhotoHolder.kt
@@ -11,7 +11,6 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-
 class PostPhotoHolder(view: View) : PostHolder(view) {
 
     private val binding = PhotoPostItemBinding.bind(itemView)
