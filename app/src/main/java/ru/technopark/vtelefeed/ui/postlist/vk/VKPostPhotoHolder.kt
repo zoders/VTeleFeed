7e800d17d9bcbe77b6443812702fdd86af6625bb
redch <sanyafil61802@gmail.com>
2021-12-08 12:06:13 +0300

    added videos

--- app/src/main/java/ru/technopark/vtelefeed/ui/postlist/vk/VKPostPhotoHolder.kt
+++ app/src/main/java/ru/technopark/vtelefeed/ui/postlist/vk/VKPostPhotoHolder.kt
@@ -1,15 +1,21 @@
 package ru.technopark.vtelefeed.ui.postlist.vk
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import ru.technopark.vtelefeed.R
 import ru.technopark.vtelefeed.data.VKPost
+import ru.technopark.vtelefeed.data.vk.VKPhoto
+import ru.technopark.vtelefeed.data.vk.VKVideo
 import ru.technopark.vtelefeed.databinding.VkPhotoPostItemBinding
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -41,37 +47,54 @@ class VKPostPhotoHolder(view: View) : VKPostHolder(view) {
                 val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
                 itemView.context.startActivity(browserIntent)
             }
-            if (post.photos != null) {
-                post.photos.forEach {
-                    val imageView = ImageView(itemView.context)
+            setPhotos(post.photos, binding)
+            setVideos(post.videos, binding)
+            val date = Date(post.date * MILLIS_IN_SECOND)
+            val dateText = SimpleDateFormat("HH:mm dd.MM.yyyy", Locale.getDefault()).format(date)
+            datePost.text = dateText
+        }
+    }
 
-                    Glide.with(itemView.context)
-                        .load(it.url)
-                        .transition(DrawableTransitionOptions.withCrossFade())
-                        .into(imageView)
-                    photos.addView(imageView)
-                }
-            }
-            if (post.videos != null) {
-                post.videos.forEach {
-                    val imageButton = ImageButton(itemView.context)
-                    //imageButton.setImageBitmap()
+    private fun setVideos(vkVideos: List<VKVideo>?, binding: VkPhotoPostItemBinding) {
+        if (vkVideos != null) {
+            with(binding) {
+                vkVideos.forEach {
+                    val view = LayoutInflater.from(itemView.context)
+                        .inflate(R.layout.video_item, videos, false)
+                    val preview = view.findViewById<ImageView>(R.id.preview)
+                    val videoTitle = view.findViewById<TextView>(R.id.video_title)
+                    videoTitle.text = it.title
                     val url = "https://vk.com/video?z=video${it.ownerId}_${it.id}"
-                    imageButton.setOnClickListener {
+                    preview.setOnClickListener {
                         val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
                         itemView.context.startActivity(browserIntent)
                     }
                     Glide.with(itemView.context)
                         .load(it.image)
                         .transition(DrawableTransitionOptions.withCrossFade())
-                        .into(imageButton)
+                        .into(preview)
 
-                    photos.addView(imageButton)
+                    videos.addView(view)
                 }
             }
-            val date = Date(post.date * MILLIS_IN_SECOND)
-            val dateText = SimpleDateFormat("HH:mm dd.MM.yyyy", Locale.getDefault()).format(date)
-            datePost.text = dateText
+        }
+    }
+
+    private fun setPhotos(vkPhotos: List<VKPhoto>?, binding: VkPhotoPostItemBinding) {
+        if (vkPhotos != null) {
+            with(binding) {
+                vkPhotos.forEach {
+                    //val imageView = ImageView(itemView.context)
+                    val view = LayoutInflater.from(itemView.context)
+                        .inflate(R.layout.photo_item, photos, false)
+                    val imageView = view.findViewById<ImageView>(R.id.post_photo)
+                    Glide.with(itemView.context)
+                        .load(it.url)
+                        .transition(DrawableTransitionOptions.withCrossFade())
+                        .into(imageView)
+                    photos.addView(view)
+                }
+            }
         }
     }
 
@@ -79,3 +102,4 @@ class VKPostPhotoHolder(view: View) : VKPostHolder(view) {
         const val MILLIS_IN_SECOND = 1000L
     }
 }
+
